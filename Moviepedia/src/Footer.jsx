9c9d8c7e19344d76799d01a91ba8e28efaf95b41
Sailesh T <saileshtrn06@gmail.com>
2025-09-29 12:15:01 +0530

    Update project code2

--- Moviepedia/src/Footer.jsx
+++ Moviepedia/src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const year = new Date().getFullYear(); // gets current year
   return (
     <footer style={{ textAlign: "center", padding: "1rem", background: "#eee", marginTop: "2rem", fontSize: "0.9rem", color: "#555" }}>
-      &copy; {year} Code Craft. All rights reserved.
+      &copy; {year} <a href="https://code-craft-pied.vercel.app/"><u>Code Craft</u></a>. All rights reserved.
     </footer>
   );
 };
